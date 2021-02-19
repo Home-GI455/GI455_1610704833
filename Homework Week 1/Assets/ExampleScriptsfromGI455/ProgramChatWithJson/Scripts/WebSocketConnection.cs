477e4bc97e72f6073c8f341212f1c3a2a2229c5c
LAPTOP-K0OFSBJ4\hp <sorawich.chot@bumail.net>
2021-02-20 02:08:49 +0700

    Send Assignment Week4

---
+++ Homework Week 1/Assets/ExampleScriptsfromGI455/ProgramChatWithJson/Scripts/WebSocketConnection.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using System;
+using UnityEngine.UI;
+
+namespace ChatWebSocketWithJson
+{
+    class MessageData
+    {
+        public string username;
+        public string message;
+    }
+    
+    public class WebSocketConnection : MonoBehaviour
+    {
+        public GameObject rootConnection;
+        public GameObject rootMessenger;
+
+        public InputField inputText;
+        public InputField inputUsername;
+        public Text sendText;
+        public Text receiveText;
+        
+        private WebSocket ws;
+
+        private string tempMessageString;
+
+        void Start()
+        {
+            rootConnection.SetActive(true);
+            rootMessenger.SetActive(false);
+        }
+
+        public void Connect()
+        {
+            string url = $"ws://127.0.0.1:16592/";
+
+            ws = new WebSocket(url);
+
+            ws.OnMessage += OnMessage;
+
+            ws.Connect();
+
+            rootConnection.SetActive(false);
+            rootMessenger.SetActive(true);
+        }
+
+        public void Disconnect()
+        {
+            if (ws != null)
+                ws.Close();
+        }
+        
+        public void SendMessage()
+        {
+            if (inputText.text == "" || ws.ReadyState != WebSocketState.Open)
+                return;
+
+            MessageData messageData = new MessageData();
+            messageData.username = inputUsername.text;
+            messageData.message = inputText.text;
+
+            string toJsonstr = JsonUtility.ToJson(messageData);
+            Debug.Log(toJsonstr);
+
+            ws.Send(inputText.text);
+            inputText.text = "";
+        }
+
+        private void OnDestroy()
+        {
+            if (ws != null)
+                ws.Close();
+        }
+
+        private void Update()
+        {
+            if(string.IsNullOrEmpty(tempMessageString) == false)
+            {
+                MessageData receiveMessageData = JsonUtility.FromJson<MessageData>(tempMessageString);
+
+                if (tempMessageString != null && tempMessageString != "")
+                {
+                    sendText.text += receiveMessageData.message + "\n";
+                }
+                else
+                {
+                    receiveText.text += receiveMessageData.message + "\n";
+                }
+
+                tempMessageString = "";
+            }
+        }
+
+        private void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            tempMessageString = messageEventArgs.Data;
+        }
+    }
+}
+
+
--- Homework Week 1/Assets/Scripts/WebSocketConnections.cs
+++ Homework Week 1/Assets/Scripts/WebSocketConnections.cs
@@ -4,117 +4,264 @@ using UnityEngine;
 using WebSocketSharp;
 using UnityEngine.UI;
 
-public class WebSocketConnections : MonoBehaviour
+namespace OnlineChatWithJson
 {
    
-
-    //Code for connect server
-    private WebSocket websocket;
-    public InputField ServerNum;
-    public InputField PortNum;
-
-    //Code for chat
-    //[SerializeField]
-    //List<Message> messagelists = new List<Message>();
-    //public GameObject TextObject;
-    //public GameObject Chatbox;
-    public InputField EnterMesseage_Word_Sentences;
-    public string text;
-    public Text TextOBJ;
-    //public GameObject TextFix;
-
-
-    public void GotoMessageChat()
+    public class WebSocketConnections : MonoBehaviour
     {
-        websocket = new WebSocket("ws://127.0.0.1:16592");
-        websocket.OnMessage += OnMessage;
-        websocket.Connect();
-        //websocket.Send("Online");
-    }
-
-
-    private void Start()
-    {
-        //SendM();
-    }
-    private void Update()
-    {
-        //SendM();
-    }
-    public void SendM()
-    {
-        text = EnterMesseage_Word_Sentences.text;
-        EnterMesseage_Word_Sentences.text = "";
-        websocket.Send(text);
-        //MessageChat(EnterMesseage_Word_Sentences.text);
-        //websocket.Send(TextFix.GetComponent<Text>().text);
-        //EnterMesseage_Word_Sentences.ActivateInputField();
-        //if (EnterMesseage_Word_Sentences.text != "")
-        //{
-        //    MessageChat(EnterMesseage_Word_Sentences.text);
-        //    websocket.Send(EnterMesseage_Word_Sentences.text);
-        //}
-        //else
-        //{
-        //    if (!EnterMesseage_Word_Sentences.isFocused)
-        //    {
-        //        EnterMesseage_Word_Sentences.ActivateInputField();
-        //    }
-        //}
-
-        //if (!EnterMesseage_Word_Sentences.isFocused)
-        //{
-        //    if (Input.GetKeyDown(KeyCode.Space))
-        //    {
-        //        MessageChat("Error not correct");
-        //    }
-        //}
-
-    }
-
-
-    private void OnDestroy()
-    {
-        if(websocket != null)
+        struct DataofMessage
         {
-            websocket.Close();
+            public string OnlineName;
+            public string Message;
+            public DataofMessage(string OnlineName, string Message)
+            {
+                this.OnlineName = OnlineName;
+                this.Message = Message;
+            }
+
         }
+
+        public struct SocketEvent
+        {
+            public string eventName;
+            public string data; //data
+
+            public SocketEvent(string eventName, string data)
+            {
+                this.eventName = eventName;
+                this.data = data;
+            }
+        }
+
+        
+        private WebSocket websocket;
+
+        public GameObject RootConnect;
+        public GameObject RootChat;
+        public GameObject rootCreateRoomWindow;
+        public GameObject rootJoinRoomWindow;
+        public GameObject rootLobby;
+        public GameObject CreateRoomFail;
+        public GameObject JoinRoomFail;
+
+        public InputField CreateRoomName;
+        public InputField JoinRoonName;
+        public InputField EnterMesseage_Word_Sentences;
+        public InputField Enter_Onlinename;
+
+        public delegate void DelegateHandle(SocketEvent result);
+        public DelegateHandle OnCreateRoom;
+        public DelegateHandle OnJoinRoom;
+        public DelegateHandle OnLeaveRoom;
+
+        public Text Sendtext;
+        public Text ReceiveText;
+        public Text Roomname;
+        private string TempMessageString;
+
+        private void Start()
+        {
+            RootConnect.SetActive(true);
+            RootChat.SetActive(false);
+            rootLobby.SetActive(false);
+            rootCreateRoomWindow.SetActive(false);
+            CreateRoomFail.SetActive(false);
+            JoinRoomFail.SetActive(false);
+            rootJoinRoomWindow.SetActive(false);
+    
+        }
+        public void Connection()
+        {
+            string url = "ws://127.0.0.1:16592/";
+            websocket = new WebSocket(url);
+            websocket.OnMessage += OnMessage;
+            
+            websocket.Connect();
+            
+            //SocketEvent socketEvent = new SocketEvent("CreateRoom","JoinRoom");
+            //string toJsonStr = JsonUtility.ToJson(socketEvent);
+            //websocket.Send(toJsonStr);
+
+            rootLobby.SetActive(true);
+            RootConnect.SetActive(false);
+        }
+
+        public void JoinRoom(string RoomName)
+        {
+
+            RoomName = JoinRoonName.text;
+            
+
+            Roomname.text = RoomName;
+            SocketEvent socketEvent = new SocketEvent("JoinRoom", RoomName);
+
+            string toJsonStr = JsonUtility.ToJson(socketEvent);
+
+            websocket.Send(toJsonStr);
+
+            //RootChat.SetActive(true);
+            //rootJoinRoomWindow.SetActive(false);
+        }
+        public void GotoCreateRoomWindow() 
+        {
+            rootLobby.SetActive(false);
+            rootCreateRoomWindow.SetActive(true);
+        }
+
+        public void GotoJoinRoom()
+        {
+            rootLobby.SetActive(false);
+            rootJoinRoomWindow.SetActive(true);
+        }
+        public void BacktoMenu()
+        {
+            rootCreateRoomWindow.SetActive(false);
+            rootJoinRoomWindow.SetActive(false);
+            rootLobby.SetActive(true);
+        }
+        //public void Backto
+        public void CreateRoom(string RoomName)
+        {
+
+             RoomName = CreateRoomName.text;
+
+            Roomname.text = RoomName;
+            SocketEvent socketEvent = new SocketEvent("CreateRoom", RoomName);
+
+            string toJsonStr = JsonUtility.ToJson(socketEvent);
+
+            websocket.Send(toJsonStr);
+
+            //RootChat.SetActive(true);
+            //rootCreateRoomWindow.SetActive(false);
+
+            //if (CreateRoomName.text == RoomName)
+            //{
+            //    UpdateNotifyMessage();
+            //    RootChat.SetActive(false);
+            //    CreateRoomFail.SetActive(true);
+            //    OnDestroy();
+            //}
+        }
+        public void LeaveRoom()
+        {
+            SocketEvent socketEvent = new SocketEvent("LeaveRoom", "");
+
+            string toJsonStr = JsonUtility.ToJson(socketEvent);
+
+            websocket.Send(toJsonStr);
+
+            
+
+        }
+        private void Update()
+        {
+            
+            UpdateNotifyMessage();
+            //if (string.IsNullOrEmpty(TempMessageString) == false)
+            //{
+            //    DataofMessage receiveMessageData = JsonUtility.FromJson<DataofMessage>(TempMessageString);
+
+            //    if (TempMessageString != null && TempMessageString != "")
+            //    {
+            //        Sendtext.text += receiveMessageData.Message + "\n";
+            //    }
+            //    else
+            //    {
+            //        ReceiveText.text += receiveMessageData.Message + "\n";
+            //    }
+
+            //    TempMessageString = "";
+            //}
+        }
+
+        private void UpdateNotifyMessage()
+        {
+            if (string.IsNullOrEmpty(TempMessageString) == false)
+            {
+                SocketEvent receiveMessageData = JsonUtility.FromJson<SocketEvent>(TempMessageString);
+
+                if (receiveMessageData.eventName == "CreateRoom")
+                {
+                    if (OnCreateRoom != null)
+                        OnCreateRoom(receiveMessageData);
+                    if (receiveMessageData.data != "fail") 
+                    {
+                        rootCreateRoomWindow.SetActive(false);
+                        RootChat.SetActive(true);
+                    }
+                    else
+                    {
+                        CreateRoomFail.SetActive(true);
+                    }
+                }
+                else if (receiveMessageData.eventName == "JoinRoom")
+                {
+                    
+                    if (OnJoinRoom != null)
+                        OnJoinRoom(receiveMessageData);
+                    if (receiveMessageData.data != "fail")
+                    {
+                        rootJoinRoomWindow.SetActive(false);
+                        RootChat.SetActive(true);
+                        
+                    }
+                    else
+                    {
+                        JoinRoomFail.SetActive(true);
+                        
+                    }
+                }
+                else if (receiveMessageData.eventName == "LeaveRoom")
+                {
+                    if (OnLeaveRoom != null)
+                        OnLeaveRoom(receiveMessageData);
+                    RootChat.SetActive(false);
+                    rootLobby.SetActive(true);
+                }
+
+                TempMessageString = "";
+            }
+
+            
+        }
+        public void Disconnect()
+        {
+            if (websocket != null)
+                websocket.Close();
+        }
+        public void SendM()
+        {
+            if (EnterMesseage_Word_Sentences.text == "" || websocket.ReadyState != WebSocketState.Open)
+                return;
+
+            DataofMessage messageData = new DataofMessage();
+            messageData.OnlineName = Enter_Onlinename.text;
+            messageData.Message = EnterMesseage_Word_Sentences.text;
+            string toJsonstr = JsonUtility.ToJson(messageData);
+            Debug.Log(toJsonstr);
+
+            websocket.Send(EnterMesseage_Word_Sentences.text);
+            EnterMesseage_Word_Sentences.text = "";
+        }
+
+
+        private void OnDestroy()
+        {
+            Disconnect();
+        }
+
+        public void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+
+            Debug.Log(messageEventArgs.Data);
+            TempMessageString = messageEventArgs.Data;
+
+        }
+        
+
+
     }
-
-    public void OnMessage(object sender, MessageEventArgs messageEventArgs)
-    {
-        //Debug.Log("Receive msg" + messageEventArgs.Data);
-        //MessageChat(text);
-        //TextObject = messageEventArgs.Data;
-        //MessageChat();
-        //SendM();
-        TextOBJ.text += "\n" + messageEventArgs.Data;
-        Debug.Log("Receive msg" + messageEventArgs.Data);
-
-    }
-
-    public void MessageChat()
-    {
-
-        text = EnterMesseage_Word_Sentences.text;
-        EnterMesseage_Word_Sentences.text = "";
-        websocket.Send(text);
-        //Message newMessage = new Message();
-        //newMessage.text = text;
-        //messagelists.Add(newMessage);
-        //GameObject newText = Instantiate(TextObject, Chatbox.transform);
-        //newMessage.TextObject = newText.GetComponent<Text>();
-        //newMessage.TextObject.text = newMessage.text;
-        //websocket.Send(text);
-    }
-
-   
 }
 
-//[System.Serializable]
-//public class Message
-//{
-//    public string text;
-//    public Text TextObject;
-//    //public string TextOBJ;
-//}
+
